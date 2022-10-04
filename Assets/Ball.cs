285806d620956dbec6549206b6622a5dfe81732c
yuukihatasawa <98634400+yuukihatasawa@users.noreply.github.com>
2022-10-04 21:55:08 +0900

    Ballに力を加える処理を付与

---
+++ Assets/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // プログラムが起動した直後、一回処理
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(100, -100, 0));//AddForse　力を加える処理
+    }
+
+    // ゲーム中繰り返し処理
+    void Update()
+    {
+        
+    }
+}
