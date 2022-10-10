504edadde7c01555f75dbaf42f91db99dc9d20e4
yuukihatasawa <98634400+yuukihatasawa@users.noreply.github.com>
2022-10-10 20:49:19 +0900

    GameClearを表示

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameClearText;//GameControllerにGameClearTextインスタンスを参照するため
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ブロックを全て習得させる
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");
+        if (blocks.Length == 0)
+        {
+            gameClearText.SetActive(true);//最初に無効にしていたチェックボックスに再びチェックをつけることができる。
+        }
+    }
+}
