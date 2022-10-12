bca14e796158fb93bffb64c442c12c6912ea72f4
yuukihatasawa <98634400+yuukihatasawa@users.noreply.github.com>
2022-10-12 22:06:51 +0900

    Retryボタンを作成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;//シーンの読み込み機能(Unityが持っているシーンを管理する機能を使うよ)
+
 
 public class GameController : MonoBehaviour
 {
     public GameObject gameClearText;//GameControllerにGameClearTextインスタンスを参照するため
     public GameObject gameOverText;//gameOverTextを受け取る変数
     public GameObject ball;//ボールの情報
+    public GameObject retryButton;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +27,21 @@ public class GameController : MonoBehaviour
             //最初に無効にしていたチェックボックスに再びチェックをつけることができる。true（SetActive）
             gameClearText.SetActive(true);
             ball.GetComponent<Rigidbody>().isKinematic = true;
+            retryButton.SetActive(true);
         }
         //ボールのBallScriptの中のisDeadフラグがTrueだったとき、GameOverTextがアクティブに
         if (ball.GetComponent<Ball>().isDead == true)
         {
             gameOverText.SetActive(true);
-            //ゲームオーバーになったとき、ボールの動きを止める処理
+            //ゲームオーバーになったとき、ボールの動きを止める処理　isKinematic→物理演算の影響
             ball.GetComponent<Rigidbody>().isKinematic = true;
+            retryButton.SetActive(true);
         }
+    }
+
+    public void Retry()
+    {
+        //シーンを読み込むための関数。どのシーンを呼び込むかは（）の中に
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
